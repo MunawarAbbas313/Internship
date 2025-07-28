3b5f5121718f22669ef3699421e5939643bd01ee
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-07-28 11:53:05 +0500

    Update Day1.js

--- Day1.js
+++ Day1.js
@@ -21,3 +21,21 @@ console.log(b); //  ReferenceError (not hoisted like var)
 let b = 20;
 
 
+//3. Scope
+function testScope() {
+  var a = 1;      // function-scoped
+  let b = 2;      // block-scoped
+  const c = 3;    // block-scoped
+
+  if (true) {
+    var a = 10;   // same variable
+    let b = 20;   // new block variable
+    const c = 30; // new block variable
+    console.log(a, b, c); // 10, 20, 30
+  }
+
+  console.log(a); // 10 (var affected)
+  console.log(b); // 2 (let not affected)
+  console.log(c); // 3 (const not affected)
+}
+testScope();
