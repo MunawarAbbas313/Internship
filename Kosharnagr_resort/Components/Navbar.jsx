832f5c204379e6412593b566448e0028b1df7cb5
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-07-31 21:43:31 +0500

    Create Navbar.jsx

---
+++ Kosharnagr_resort/Components/Navbar.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { RxHamburgerMenu, RxCross1 } from "react-icons/rx";
+import data from "../Data/data.json"
+
+function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      {/* Navbar container */}
+      <div className="fixed top-0 left-0 w-full bg-white  p-7 flex justify-around items-center border-b border-gray-200 shadow-sm z-[100] md:gap-[75px] ">
+        
+        {/* Logo */}
+        <div className="text-2xl text-black font-bold font-serif">{data.companyInfo.name}</div>
+
+        {/* Desktop Nav */}
+        <div className="hidden md:block">
+          <ul className="flex gap-10 text-lg items-center">
+            {data.navigation.map((item, i) => (
+              <li key={i} className="relative group cursor-pointer text-black">
+                <span className="transition duration-200 group-hover:text-slate-400">{item}</span>
+                <span className="absolute left-0 -bottom-1 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* Hamburger Icon */}
+        <div
+          className="md:hidden text-3xl cursor-pointer z-50"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        >
+          {isMenuOpen ? <RxCross1 /> : <RxHamburgerMenu />}
+        </div>
+      </div>
+
+      {/* Mobile Menu - Slides from Top */}
+      <div
+        className={`absolute top-16 left-0 w-full bg-white border-y border-gray-200 shadow-md transition-all duration-300 ease-in-out z-[90] ${
+          isMenuOpen ? "opacity-100 translate-y-0" : "opacity-0 -translate-y-10 pointer-events-none"
+        } md:hidden`}
+      >
+        <ul className="flex flex-col gap-4 px-6 py-6 text-lg font-medium">
+          {data.navigation.map((item, i) => (
+            <li
+           key={i}
+               className="group cursor-pointer text-black border border-gray-400 rounded-xl w-full px-4 py-3 bg-gray-50 hover:bg-gray-100 transition-all relative overflow-hidden"
+         >
+            <span className="transition duration-200 group-hover:text-slate-500 z-10 relative">{item}</span>
+  
+         {/* underline animation */}
+               <span className="absolute left-0 bottom-0 h-[2px] w-0 bg-black transition-all duration-300 group-hover:w-full"></span>
+           </li>
+
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
