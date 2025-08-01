19ed8b02cd80786a55f2c2e472e6a7252a09bebd
MunawarAbbas313 <153192634+MunawarAbbas313@users.noreply.github.com>
2025-08-01 20:39:34 +0500

    Update Home.jsx

--- Kosharnagr_resort/Pages/Home.jsx
+++ Kosharnagr_resort/Pages/Home.jsx
@@ -1,1 +1,32 @@
 
+import About from "../Components/About";
+import Hero from "../Components/Hero";
+import Navbar from "../Components/Navrbar";
+import Resort from "../Components/Resort";
+import Location from "../Components/Location"
+import Review from "../Components/Review"
+import Footer from "../Components/Footer"
+
+
+
+function Home(){
+    return(
+        <div className="overflow-hidden"> 
+        <div className=" relative">
+            <Navbar/>
+             <Hero/>
+           
+        </div>
+        <div className="mt-12">
+              <About/> 
+              <Resort/>
+              <Location/>
+              <Review/>
+              <Footer/>
+              
+        </div>
+         
+        </div>
+    )
+}
+export default Home;
